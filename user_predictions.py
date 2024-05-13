a1657884a086661f43f13bde85291da9034a3d5c
bkilpatrick-fullerton <158365201+bkilpatrick-fullerton@users.noreply.github.com>
2024-05-12 20:32:36 -0700

    Refactor user_predictions.py

--- user_predictions.py
+++ user_predictions.py
@@ -29,8 +29,20 @@ REVIEWS_SCHEMA = StructType([
             StructField("userId", IntegerType(), True),
         ])
 
+USERS_SCHEMA = StructType([
+            StructField("userId", IntegerType(), True),
+            StructField("userName", StringType(), True),
+            StructField("cnt_reviews", IntegerType(), True),
+            StructField("avg_rating", FloatType(), True),
+
+        ])
+
+
 
 def get_users(per_page, page):
+    """GET_USERS"""
+
+    # create spark session
     print("creating spark session ... ")
     spark = SparkSession.builder \
         .master('local[*]') \
@@ -38,20 +50,27 @@ def get_users(per_page, page):
         .appName("CSV Read and Write") \
         .getOrCreate()
 
+    # read in users file
     print("reading users files .. ")
     df_users = spark.read.csv(USERS_FILE, header=True)
-    # df_users = df_users.limit(per_page)
+
+    # find starting record for next page
     skip = per_page * (page - 1)
 
+    # only pull records for the "next" page
     df_users = df_users.where(col('userId').between(skip, skip + per_page))
 
-    # df_users = df_users.limit(per_page).skip(skip)
-    df_users.show()
+    if SHOW:
+        print("df_users:")
+        df_users.show()
 
     return df_users.toJSON().collect()
 
 
 def search_users(search_key):
+    """SEARCH_USERS"""
+
+    # create spark session
     print("creating spark session ... ")
     spark = SparkSession.builder \
         .master('local[*]') \
@@ -59,33 +78,67 @@ def search_users(search_key):
         .appName("searching users") \
         .getOrCreate()
 
+    # read in users file
     print("reading users files .. ")
     df_users = spark.read.csv(USERS_FILE, header=True)
-    # df_users = df_users.limit(per_page)
 
-    # df_users = df_users.where(col('userName').between(skip, skip + per_page))
+    # filter on single userName
     results = df_users.filter(df_users['userName'] == search_key)
-    # df_users = df_users.limit(per_page).skip(skip)
-    results.show()
+
+    if SHOW:
+        print("results:")
+        results.show()
 
     return results.toJSON().collect()
 
+
+def load_csv_into_df(path, schema, spark):
+    """LOAD GAMES"""
+    df = spark.read.csv(path, schema=schema, header=True)
+    return df
+
+def start_spark_session():
+    """START_SPARK_SESSION"""
+    spark = SparkSession.builder \
+        .master('local[*]') \
+        .config("spark.driver.memory", "3g") \
+        .appName("single_user_predictor") \
+        .getOrCreate()
+    return spark
+
+
+def lookup_userid_byusername(userName):
+    '''LOOKUP_USERID_BYUSERNAME'''
+    print("creating spark session: ")
+    spark = start_spark_session()
+
+    try:
+        print("loading users file ...")
+        df_users = load_csv_into_df(USERS_FILE, USERS_SCHEMA, spark)
+
+        print(f"finding user {userName}...")
+        first_row = df_users.filter(col("userName") == userName).first()
+        userId = first_row['userId']
+
+    finally:
+        print("spark stop")
+        spark.stop()
+
+    return userId
 
 class UserPredictions:
     def __init__(self):
         self.userId = 66439
         self.userName = "ioneskylab"
         self.min_num_ratings = 200
+        self.max_predictions = 20
         self.exclude_rated_games = False
         self.predictions = []
+        self.predictions_list = []
 
-    def load_csv_into_df(self, path, schema, spark):
-        """LOAD GAMES"""
-        df = spark.read.csv(path, schema=schema, header=True)
-        return df
 
     def generate_predictions(self, ):
-
+        """GENERATE PREDICTIONS"""
         success_flag = False
 
         # If not, create a SparkSession
@@ -96,30 +149,35 @@ class UserPredictions:
             .getOrCreate()
 
         try:
+            #load in games file
             print("loading games file ...")
-            df_games = self.load_csv_into_df(GAMES_FILE, GAMES_SCHEMA, spark)
+            df_games = load_csv_into_df(GAMES_FILE, GAMES_SCHEMA, spark)
+
+            # filter out games with too few reviews (eliminates obscure/new/unreliable games)
             df_games = df_games.filter(col("cnt_reviews") >= self.min_num_ratings)
 
             if SHOW:
                 print("df_games:")
                 df_games.show(10)
 
-
+            # load reviews file
             print("loading reviews file ...")
-            df_reviews = self.load_csv_into_df(REVIEWS_FILE, REVIEWS_SCHEMA, spark)
+            df_reviews = load_csv_into_df(REVIEWS_FILE, REVIEWS_SCHEMA, spark)
+
+            # filter down to just our single user
             df_reviews = df_reviews.filter(col("userID") == self.userId)
 
             if SHOW:
                 print("df_reviews:")
                 df_reviews.show(10)
 
-            # join the two datasets
+            # join the two datasets on gameId
             print("joining games and reviews ...")
             df_joined = df_games.alias('g').join(df_reviews.alias('r'), col('g.gameId') == col('r.gameId'), 'left') \
 
 
+            # Add in our UserId column (the predictor needs it) and substitute -1 for missing ratings (non-rated games)
             print("selecting columns for final df ...")
-            # Select all columns from 'games' and 'rating' from 'reviews', replacing null with 0
             df_predict_user = (df_joined.select('g.*'
                                                 , lit(self.userId).alias('userId')
                                                 , lit(self.userName).alias('userName')
@@ -127,6 +185,7 @@ class UserPredictions:
 
                                )
 
+            # remove games that have already been rated (default)
             if self.exclude_rated_games:
                 df_predict_user = df_predict_user.filter(col("rating") == -1)
 
@@ -138,7 +197,7 @@ class UserPredictions:
             print("loading ALS model ...")
             als_model = ALSModel.load(MODEL_PATH)
 
-            # Generate predictions
+            # Generate predictions. Order by prediction DESC (best predictions first)
             print("generating predictions ...")
             df_predictions = als_model.transform(df_predict_user)
             df_predictions = df_predictions.orderBy(desc("prediction"), "rating")
@@ -147,14 +206,15 @@ class UserPredictions:
                 print("df_predictions:")
                 df_predictions.show(10)
 
-            # Extract predictions
+            # Save prediction as list (for debug)
+            self.predictions_list = df_predictions.collect()
+
+            # Extract predictions (default top 20)
             print("collect predictions in to list")
-            top_20_rows = df_predictions.limit(20)
-            #
-            # # Convert the top 20 rows to JSON
-            # self.predictions = [row.toJSON() for row in top_20_rows]
-            self.predictions = df_predictions.collect()
-            self.predictions = top_20_rows.toJSON().collect()
+            top_X_rows = df_predictions.limit(self.max_predictions)
+
+            # Save predictions as JSON (for UI)
+            self.predictions = top_X_rows.toJSON().collect()
 
             success_flag = True
 
@@ -172,12 +232,18 @@ class UserPredictions:
 # Example usage
 if __name__ == "__main__":
 
+    userName = "ioneskylab"
+    userId = lookup_userid_byusername(userName)
+
     user = UserPredictions()
-    user.userId = 66439
-    user.userName = "ioneskylab" # (optional) just includes it in the output dataset (this is me BTW)
+    user.userId = userId
+    user.userName = userName
 
     if user.generate_predictions():
         for prediction in user.predictions[:20]:
             print(prediction)
 
+    # foo = get_users(10, 1)
+    #foo = search_users(userName)
+    #print(foo)
 
