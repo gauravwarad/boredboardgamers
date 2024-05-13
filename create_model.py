866b3b4be5c7c2a86803e0826dced4cef017d69c
bkilpatrick-fullerton <158365201+bkilpatrick-fullerton@users.noreply.github.com>
2024-05-12 20:26:13 -0700

    Refactor create_model.py

--- create_model.py
+++ create_model.py
@@ -1,46 +1,75 @@
-from pyspark.ml.evaluation import RegressionEvaluator
+
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import hour, col, count, sum
-from pyspark.ml.feature import OneHotEncoder, StringIndexer
-from pyspark.sql.window import Window
 
 import findspark
 findspark.init()
 
-INPUT_FILE = "hdfs_datastore/csv_bgReviews"
+DEBUG = True
+
+REVIEWS_FILE = "hdfs_datastore/csv_bgReviews"
 MODEL_PATH = "hdfs_datastore/model_bgPredictions"
 
-spark = SparkSession.builder \
-    .master('local[*]') \
-    .config("spark.driver.memory", "3g") \
-    .appName("bgrecommender") \
-    .getOrCreate()
+RANK_OVERRIDE= 10  # Enter the Rank value from the crossvalidator "best model"
+REGPARAM_OVERRIDE = 0.1  # Enter the RegParam value from the crossvalidator "best model"
 
-
-bggreviewsSchema = StructType([
+REVIEWS_SCHEMA = StructType([
     StructField("userName", StringType(), True),
     StructField("gameId", IntegerType(), True),
     StructField("rating", FloatType(), True),
     StructField("gameName", StringType(), True),
     StructField("userId", IntegerType(), True),
-
-
 ])
 
 
-df = spark.read.csv(INPUT_FILE, schema=bggreviewsSchema, header=True)
-df_count = df.count()
-print(f"loaded file: {INPUT_FILE} ({df_count} rows)")
-print("sample:")
-df.show(20)
+def load_csv_into_df(path, schema, spark):
+    """LOAD GAMES"""
+    df = spark.read.csv(path, schema=schema, header=True)
+    return df
 
-#Create Model using ALS (alternating least squares method)
-als = ALS(userCol="userId", itemCol="gameId", ratingCol="rating", coldStartStrategy="drop")
-model = als.fit(df)
 
-#Save Model in hdfs for reuse
-model.save(MODEL_PATH)
+def start_spark_session():
+    """START SPARK SESSION"""
+    spark = SparkSession.builder \
+        .master('local[*]') \
+        .config("spark.driver.memory", "3g") \
+        .appName("single_user_predictor") \
+        .getOrCreate()
+    return spark
 
-spark.stop()
+
+def main():
+    """MAIN"""
+    # start spark session
+    print("starting spark session...")
+    spark = start_spark_session()
+
+    try:
+        # load full reviews file
+        print("loading raw reviews file...")
+        df_reviews = load_csv_into_df(REVIEWS_FILE, REVIEWS_SCHEMA, spark)
+
+        if DEBUG:
+            df_count = df_reviews.count()
+            print(f"loaded file: {REVIEWS_FILE} ({df_count} rows)")
+            print("df_reviews:")
+            df_reviews.show(20)
+
+        #Create Model using ALS (alternating least squares method)
+        print("creating als model...")
+        als = ALS(rank=RANK_OVERRIDE, regParam=REGPARAM_OVERRIDE, userCol="userId", itemCol="gameId", ratingCol="rating", coldStartStrategy="drop")
+        model = als.fit(df_reviews)
+
+        #Save Model in hdfs for reuse
+        print("saving als model...")
+        model.save(MODEL_PATH)
+
+    finally:
+        # either way stop spark
+        print("spark stop")
+        spark.stop()
+
+
+if __name__ == "__main__":
+    main()
